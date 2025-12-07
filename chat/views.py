c1912dc24e35e7507d93a521c3eab58a91f49151
TomaszWojcicki <Axerrek26@gmail.com>
2025-12-07 17:33:03 +0100

    Drobne poprawki

--- chat/views.py
+++ chat/views.py
@@ -20,6 +20,12 @@ def chat_detail(request, thread_id):
         # Odświeżenie strony po wysłaniu wiadomości
         return redirect('chat_detail', thread_id=thread.id)
     
+    # pobieramy wszystkie czaty użytkownika (listę po lewej stronie)
+    threads = ChatThread.objects.filter(users=request.user)
+    threads_with_other = [
+        (t, t.users.exclude(id=request.user.id).first())
+        for t in threads
+    ]
     messages = thread.messages.all().order_by("timestamp")
     
     # Lista wątków po lewej stronie
@@ -68,16 +74,3 @@ def chat_list(request):
         threads_with_other.append((t, other))
     
     return render(request, "chat/chat_list.html", {"threads_with_other": threads_with_other})
-@login_required
-def chat_detail(request, thread_id):
-    thread = get_object_or_404(ChatThread, id=thread_id, users=request.user)
-    messages = thread.messages.all().order_by("timestamp")
-    
-    # Wyznaczamy drugiego użytkownika
-    other_user = thread.users.exclude(id=request.user.id).first()
-    
-    return render(request, "chat/chat_detail.html", {
-        "thread": thread,
-        "messages": messages,
-        "other_user": other_user
-    })
